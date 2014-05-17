8502e28eddf1f29d5d699069da1a03d3ad401296
orian <pawelszczur@gmail.com>
2014-05-17 23:27:13 +0200

    Enable remote_api in app.

--- backend/backend.go
+++ backend/backend.go
@@ -1,11 +1,12 @@
 package backend
 
 import (
+	_ "appengine/remote_api"
 	"backend/handlers"
 	"net/http"
 )
 
 func init() {
-  http.HandleFunc("/", handlers.Main)
+	http.HandleFunc("/", handlers.Main)
 	http.HandleFunc("/helloWorld", handlers.HelloWorld)
 }
